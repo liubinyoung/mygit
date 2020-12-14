b118cde9075a956c23b28529a1791d74813cbfec
liubinyoung <chian.liubingyang@outlook.com>
2020-12-14 21:17:45 +0800

    TestGit01,第二次提交。

--- src/main/java/com/lby/mygit/TestGit01.java
+++ src/main/java/com/lby/mygit/TestGit01.java
@@ -3,5 +3,6 @@ package com.lby.mygit;
 public class TestGit01 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
